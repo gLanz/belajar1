7c46218e98edda3d5a987bbc98d69c9232558fcc
Faber Nainggolan <goeriks@gmail.com>
2024-01-17 22:53:51 +0700

    Initial commit

---
+++ application/views/menu.php
@@ -0,0 +1,5 @@
+    <a href="./Home">Home</a> |
+    <a href="./Profil">Profil</a> |
+    <a href="./Contact">Kontak</a> |
+    <a href="./Hubungi">Hubungi Kami</a> |
+    <a href="./Berita">Berita</a>
